39af8a1555e55061dac849d629a5fe0890472a26
jppalomo10 <131483959+jppalomo10@users.noreply.github.com>
2025-12-18 09:39:09 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from database import crear_tablas
+
+crear_tablas()
+
+st.set_page_config(
+    page_title="COIMPEX S.A.",
+    page_icon="🍇",
+    layout="wide"
+)
+
+st.title("🏠 Menú principal")
+
+st.markdown("""
+Bienvenido a la aplicación.
+Selecciona una sección desde el menú lateral.
+""")
