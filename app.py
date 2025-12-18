8ad07a62fb078c6066b719ad6f6ade224caa9066
jppalomo10 <131483959+jppalomo10@users.noreply.github.com>
2025-12-18 10:10:37 -0600

    Refactor Streamlit app layout and configuration

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 import streamlit as st
 from database import crear_tablas
 
-import streamlit as st
-
-USER = st.secrets["auth"]["username"]
-PASS = st.secrets["auth"]["password"]
-
-st.title("Login")
-
-user = st.text_input("Usuario")
-password = st.text_input("Contraseña", type="password")
-
-if st.button("Ingresar"):
-    if user == USER and password == PASS:
-        st.success("Bienvenido")
-        st.session_state["logged"] = True
-    else:
-        st.error("Credenciales incorrectas")
-
-if st.session_state.get("logged"):
-    st.write("Aquí va tu app 🔐")
-
 st.set_page_config(
     page_title="COIMPEX S.A.",
     page_icon="🍇",
@@ -33,4 +13,5 @@ st.markdown("""
 Bienvenido a la aplicación.
 Selecciona una sección desde el menú lateral.
 """)
+
 
