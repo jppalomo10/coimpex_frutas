d386371ace2e1fd0a033d8dbac046d066c372670
jppalomo10 <131483959+jppalomo10@users.noreply.github.com>
2025-12-19 21:37:51 -0600

    Add inventory queries and display in Streamlit app
    Added queries for clientes, proveedores, encabezados, and detalles. Implemented inventory query and displayed current inventory in a dataframe.

--- app.py
+++ app.py
@@ -18,3 +18,31 @@ st.subheader("Estado de la base de datos")
 
 row = run_query("select now() as ahora;", fetch="one")
 st.write("Conectado ✅", row["ahora"])
+
+clientes = run_query("SELECT * FROM clientes")
+proveedores = run_query("SELECT * FROM proveedores")
+encabezados = run_query("SELECT * FROM encabezados")
+detalle = run_query("SELECT * FROM detalles")
+
+query_inventario = """
+SELECT
+    d.sku,
+    SUM(CASE WHEN e.transaccion = 2 THEN d.cantidad ELSE 0 END) AS entradas,
+    SUM(CASE WHEN e.transaccion = 1 THEN d.cantidad ELSE 0 END) AS salidas,
+    SUM(
+        CASE
+            WHEN e.transaccion = 2 THEN d.cantidad
+            WHEN e.transaccion = 1 THEN -d.cantidad
+            ELSE 0
+        END
+    ) AS stock_actual
+FROM detalles d
+JOIN encabezados e ON d.id_transaccion = e.id_transaccion
+GROUP BY d.sku
+ORDER BY d.sku;
+"""
+
+inventario = run_query(query_inventario, fetch="all")
+
+st.subheader("Inventario Actual")
+st.dataframe(inventario, use_container_width=True)
